96de159750c1b947b672a798f03c720286bb59f5
safwanmecayle <safwanhasanmecayle@gmail.com>
2021-03-29 15:03:42 +0200

    ok

---
+++ Truthy-Falsy.js
@@ -0,0 +1,28 @@
+// falsy values
+// ""
+// 0
+// undefined
+// null
+// NaN
+// truthy values
+// '0' ' '
+
+let age = 5;
+
+if (age > 3 ) {
+    console.log ("condition is true");
+} 
+else {
+    console.log("condition is Flase");
+}
+
+// let nameName = [10];
+for (let i = 1; i <= 10; i++) {
+    console.log ("ALLAH");
+}
+sum=[10,20,30,40,50];
+
+for (let index = 0; index <= 3; index++) {
+    const element = sum[index]; 
+    console.log(element);  
+}
