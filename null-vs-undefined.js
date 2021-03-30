51073d9df5f6c7dc07adced2e040cec0599331ed
safwanmecayle <safwanhasanmecayle@gmail.com>
2021-03-30 13:51:33 +0200

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+   //  undefined
+   
+// let sum
+
+// console.log(sum);
+
+// function add(num1, num2){
+//     console.log (num1 + num2);
+//     // return;
+// }
+// const result = add(10 , 20);
+// console.log(result);
+
+// function add(num1, num2){
+//         console.log (num2);
+//         // return;
+//     }
+    // const result = add(10);
+    // console.log(result);
+    
+
+// const loverBoy = { name:"safwan", phone: 667707};
+// console.log(loverBoy.gf);
+// console.log(typeof loverBoy);
+
+// let fun = undefined;
+// console.log(fun);
+
+// let ages = [2,4,5];
+// console.log(ages[10]);
+
+// null when there is no number or empty or blank.
