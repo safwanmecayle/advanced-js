fce13b07552e2843ff8363d614e15338d5a3df3f
safwanmecayle <safwanhasanmecayle@gmail.com>
2021-03-30 15:18:33 +0200

    double-vs-triple-equals

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,22 @@
+// const first = 2;
+// const second = 5;
+// const second = 2;
+// const second = "2";
+// const second = 2;
+// const first = 0;
+const first = 1;
+const second = true;      
+// if (first == second) 
+   if (first == second) {
+    console.log("Condition is True");
+}
+else {
+    console.log("Condition is False");
+
+}
+
+// == is not as strict as ===
+// so == it only checks values but 
+// === checks both values and Strings . 
+// [false = 0];
+// [true = 1];
